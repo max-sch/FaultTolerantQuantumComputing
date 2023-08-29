a25cf243342b03e2af46b3462cf5a45b22596f3e
Zakariea Sharfeddine <94124954+Zasha01@users.noreply.github.com>
2023-08-29 13:00:53 +0200

    give circuits unique names
    it is important to be able to differentiate circuits used for varying transpilation seeds, different opt levels and different backends

--- ftqc/core/qchannels.py
+++ ftqc/core/qchannels.py
@@ -34,6 +34,7 @@ class VaryingTranspilationSeedGeneration(QuantumRedundancyChannel):
         
     def create_variant_of(self, circuit):
         print("Apply varying transpilation seed channel")
+        circuit.qiskit_circuit.name = f"{circuit.id}-{self.seed}"
         return transpile(circuit.qiskit_circuit, 
                          backend=self.device.get_backend(), 
                          seed_transpiler=self.seed)
@@ -55,6 +56,7 @@ class HeterogeneousQuantumDeviceBackend(QuantumRedundancyChannel):
 
     def create_variant_of(self, circuit):
         print("Apply heterogeneous quantum device channel")
+        circuit.qiskit_circuit.name = f"{circuit.id}-{self.device}"
         return transpile(circuit.qiskit_circuit, 
                   backend=self.device.get_backend(), 
                   seed_transpiler=DEFAULT_SEED)
@@ -77,6 +79,7 @@ class DifferentOptimizationLevel(QuantumRedundancyChannel):
 
     def create_variant_of(self, circuit):
         print("Apply different optimization level channel")
+        circuit.qiskit_circuit.name = f"{circuit.id}-{DEFAULT_SEED}-{self.opt_level}"
         return transpile(circuit.qiskit_circuit, 
                          backend=self.device.get_backend(), 
                          optimization_level=self.opt_level, 
