521d459c23dc0fdd686733cf5063ade97fbd9891
Max Scheerer <scheerer@fzi.de>
2023-08-31 15:21:46 +0200

    Minor bug fixing: implement groupby logic manually

--- ftqc/evaluation/exp_eval.py
+++ ftqc/evaluation/exp_eval.py
@@ -1,13 +1,11 @@
 import datetime
 import pandas as pd
-from enum import Enum
 from evaluation.metrics import NumberOfCorrectCircuits, NumberOfTopTenCircuits, DirectComparison
 from tabulate import tabulate
-from itertools import groupby
 from os.path import join 
 
 class ApproachResult:
-    def __init__(self, results_for_approach, approach):
+    def __init__(self, approach, results_for_approach):
         self.results = results_for_approach
         self.approach = approach
         self.num_correct_evaluator = NumberOfCorrectCircuits()
@@ -23,22 +21,25 @@ class ApproachResult:
         #TODO: refatoring using enum
         return [[self.approach, "Avg", self.num_correct_evaluator.evaluate(avg_pos_results), self.num_top_ten_evaluator.evaluate(avg_pos_results, top_ten_size), self.direct_comparison_evaluator.evaluate(avg_pos_results, agg_pos_results)],
             [self.approach, "Agg", self.num_correct_evaluator.evaluate(agg_pos_results), self.num_top_ten_evaluator.evaluate(agg_pos_results, top_ten_size), "-"],
-            [self.approach, "Closest", self.num_correct_evaluator.evaluate(fixed_pos_results), self.num_top_ten_evaluator.evaluate(fixed_pos_results, top_ten_size), self.direct_comparison_evaluator.evaluate(agg_pos_results, fixed_pos_results)]]
+            [self.approach, "Fixed", self.num_correct_evaluator.evaluate(fixed_pos_results), self.num_top_ten_evaluator.evaluate(fixed_pos_results, top_ten_size), self.direct_comparison_evaluator.evaluate(agg_pos_results, fixed_pos_results)]]
         
 
 class FtqcExperimentEvaluator:
     def __init__(self, results, result_directory) -> None:
-        self.results = results
         self.result_directory = result_directory
+        self.approaches = {}
+        for result in results:
+            if result.ft_qcontainer not in self.approaches.keys():
+                self.approaches[result.ft_qcontainer] = []
+
+            self.approaches[result.ft_qcontainer].append(result)
 
     def evaluate(self):
         evaluations = {}
+        for approach, results in self.approaches.items():
+            evaluations[approach] = ApproachResult(approach, results).evaluate()
 
-        approaches = groupby(self.results, lambda x: x.ft_qcontainer)
-        for approach, results in approaches:
-            evaluations[approach] = ApproachResult(results, approach).evaluate()
-
-        return self.print_and_save(evaluations, self.result_directory)
+        return FtqcExperimentEvaluator.print_and_save(evaluations, save_directory=self.result_directory)
             
     def print_and_save(evaluations, save_directory="."):
         df = []
