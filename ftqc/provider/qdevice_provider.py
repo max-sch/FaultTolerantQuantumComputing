13fbb330df9000817e8f85ec335d7bd36d693741
Max Scheerer <scheerer@fzi.de>
2023-09-21 15:55:22 +0200

    Bug fixing: fakeproviders are using simulators which lead to deviations in the device name (especially when querying the job size).

--- ftqc/provider/qdevice_provider.py
+++ ftqc/provider/qdevice_provider.py
@@ -1,6 +1,7 @@
 from qiskit.test.mock import FakeProvider
 from qiskit import IBMQ
 from core.entities import IBMQuantumComputer, QuantumComputerSimulator
+from qiskit_ibm_provider import IBMProvider
 
 class QuantumDeviceProvider:
     def __init__(self, backend_name, ibmq_credentials=None) -> None:
@@ -9,7 +10,7 @@ class QuantumDeviceProvider:
             backend = self.provider.get_backend(backend_name if backend_name != "ibmq_ehningen" else "fake_boeblingen")
         else:
             ibmq_credentials.activate_account()
-
+            
             self.provider = IBMQ.get_provider(project="ticket")
             backend = self.provider.get_backend(backend_name)
 
@@ -26,6 +27,13 @@ class FakeQuantumDeviceProvider(QuantumDeviceProvider):
     '''The device provider is mainly considered for testing purposes'''
     def __init__(self) -> None:
         super().__init__("fake_boeblingen")
+
+    def max_job_size_for(self, device):
+        for b in self.provider.backends():
+            if b.name() in device.unique_name:
+                return b.configuration().max_experiments
+        
+        raise Exception("There is no backend for device: " + device.unique_name)
 
     def provided_devices(self, min_qubits=10):
         return [QuantumComputerSimulator.create_noisy_simulator(b) 
